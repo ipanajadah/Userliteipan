2615b29a03b3e98d6681fda837a1121857acd9b4
miko1305 <miko.yhs@gmail.com>
2025-04-24 22:02:09 +0700

    first commit

---
+++ PyroUbot/config.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+MAX_BOT = int(os.getenv("MAX_BOT", "30"))
+
+DEVS = list(map(int, os.getenv("DEVS", "8099519433").split()))
+
+API_ID = int(os.getenv("API_ID", "24001881"))
+
+API_HASH = os.getenv("API_HASH", "264ca8282af65b74b8414773ff269c5d")
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7987834818:AAGY3hbgm0SW2ngLoxknuC9nDT0LKhM33bc")
+
+OWNER_ID = int(os.getenv("OWNER_ID", "8099519433"))
+
+BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002692266809").split()))
+
+RMBG_API = os.getenv("RMBG_API", "a6qxsmMJ3CsNo7HyxuKGsP1o")
+
+MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://ipanalkenzi:ipanalkenzi@cluster0.tyhf7ap.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1002692266809"))
---
+++ README.md
@@ -0,0 +1,33 @@
+```
+apt update && apt upgrade -y
+```
+```
+git clone https://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@github.com/ipan1434/ubotlite
+```
+```
+cd ubotlite && screen -S ubotlite
+```
+```
+bash installnode.sh && apt install python3.10-venv
+```
+```
+python3 -m venv ubotlite && source ubot/bin/activate
+```
+```
+pip3 install -r requirements.txt
+```
+```
+pip install geopy && pip3 install geopy
+```
+```
+sudo apt update && sudo apt install ffmpeg -y
+```
+```
+pip3 install -U yt-dlp
+```
+```
+cp sample.env .env && nano .env
+```
+```
+python3 -m PyroUbot
+```
