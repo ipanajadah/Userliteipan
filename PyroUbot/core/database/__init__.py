9661c83f191eb5736e728a08faf812082f7e9dc6
ipanajadah <ivanpisang5@gmail.com>
2025-06-22 22:09:50 +0700

    Delete PyroUbot directory

--- PyroUbot/core/database/__init__.py
+++
@@ -1,2 +0,0 @@
-from .variabel import dB, db_path
-from .state import state
--- PyroUbot/core/database/antigcast.py
+++
@@ -1,8 +0,0 @@
-from PyroUbot.core.database import dB, db_path
-
-db = dB_client["db_path"]
-user_collection = db["tes.db"]
-
-async def get_user_ids(client_id: int):
-    user_ids = await user_collection.find_one({"_id": client_id})
-    return user_ids["tes.db"] if user_ids else []
